57072f7830bb63ec112fd08654555311e0816e87
Amit Chudasama <amit.chudasama@gmail.com>
2017-11-24 09:30:09 +0530

    corrected sql

--- server/src/main/java/org/aksdroid/play/perfectwebapp/repositories/EmployeeRepository.java
+++ server/src/main/java/org/aksdroid/play/perfectwebapp/repositories/EmployeeRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-	@Query("SELECT er.id.relatedEmployee FROM Employee e , EmployeeRelation er WHERE e.employeeId=:employeeId AND er.id.employeeId=e.employeeId")
+	@Query("SELECT er.id.relatedEmployee FROM EmployeeRelation er WHERE er.id.employeeId=:employeeId")
 	public List<Employee> findRelatedEmployees(@Param("employeeId") Integer employeeId);
 
 }
